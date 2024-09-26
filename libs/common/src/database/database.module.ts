586ba8a23741719fdfca5596ce34e43c1c409f52
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-26 12:20:37 +0330

    build: database module

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get<string>("MONGODB_URI")
+            }),
+            inject: [ConfigService]
+        })
+    ]
+})
+export class DatabaseModule { }
