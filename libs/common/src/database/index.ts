74ab13ec1e3bbac3b0542e1373e9506468992b26
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-26 12:24:07 +0330

    export: db barrel exports

---
+++ libs/common/src/database/index.ts
@@ -0,0 +1,6 @@
+export * from "./base-entity.repository"
+export * from "./database.module"
+export * from "./entity-schema.factory"
+export * from "./entity.factory"
+export * from "./entity.repository"
+export * from "./identifiable-entity.schema"
