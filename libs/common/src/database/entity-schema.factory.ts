40622d89173b3365baa70c78cdf3759b76ffea5d
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-26 12:21:13 +0330

    build: Entity Schema Factory

---
+++ libs/common/src/database/entity-schema.factory.ts
@@ -0,0 +1,10 @@
+import { AggregateRoot } from '@nestjs/cqrs';
+import { IdentifiableEntitySchema } from './identifiable-entity.schema';
+
+export interface EntitySchemaFactory<
+  TSchema extends IdentifiableEntitySchema,
+  TEntity extends AggregateRoot
+> {
+  create(entity: TEntity): TSchema;
+  createFromSchema(entitySchema: TSchema): TEntity;
+}
